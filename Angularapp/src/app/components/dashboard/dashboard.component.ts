f52691436e8a25e945a1f08d7f24fb0ebf975fb5
Bogdan Pop <Bogdan@rnce.local>
2018-01-30 10:45:42 +0000

    ading videos list

--- Angularapp/src/app/components/dashboard/dashboard.component.ts
+++ Angularapp/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +11,30 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
   user:Object;
-  constructor(private router:Router) { }
+  data:any=[];
+  private apiurl = 'http://localhost:3000/videos'
+  
+  constructor(private router:Router,
+    private http: Http  ) {
+      this.getVideos();
+      this.getData();
+     }
 
   ngOnInit() {
     const user = JSON.parse(localStorage.getItem('user'));
     this.user = user[0];
     
     
+    
   }
-
+  getData(){
+    return this.http.get(this.apiurl)
+    .map((res:Response) => res.json())
+  }
+getVideos(){
+  this.getData().subscribe(data =>{
+    console.log(data);
+    this.data = data;
+  })
+}
 }
